30ae37c043910906c5624babedf5b94cd05bb85e
Lucas Pereira Vasconcelos <lucas.pv@gilp.studio>
2020-07-01 16:50:47 -0300

    Fixed positioning on new Unity theme

--- Editor/ToolbarExtender.cs
+++ Editor/ToolbarExtender.cs
@@ -28,7 +28,9 @@ namespace UnityToolbarExtender
 			FieldInfo toolIcons = toolbarType.GetField(fieldName,
 				BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
 			
-#if UNITY_2019_1_OR_NEWER
+#if UNITY_2019_3_OR_NEWER
+			m_toolCount = toolIcons != null ? ((int) toolIcons.GetValue(null)) : 8;
+#elif UNITY_2019_1_OR_NEWER
 			m_toolCount = toolIcons != null ? ((int) toolIcons.GetValue(null)) : 7;
 #elif UNITY_2018_1_OR_NEWER
 			m_toolCount = toolIcons != null ? ((Array) toolIcons.GetValue(null)).Length : 6;
@@ -40,7 +42,11 @@ namespace UnityToolbarExtender
 			ToolbarCallback.OnToolbarGUI += OnGUI;
 		}
 
+#if UNITY_2019_3_OR_NEWER
+		public const float space = 8;
+#else
 		public const float space = 10;
+#endif
 		public const float largeSpace = 20;
 		public const float buttonWidth = 32;
 		public const float dropdownWidth = 80;
@@ -68,7 +74,11 @@ namespace UnityToolbarExtender
 			Rect leftRect = new Rect(0, 0, screenWidth, Screen.height);
 			leftRect.xMin += space; // Spacing left
 			leftRect.xMin += buttonWidth * m_toolCount; // Tool buttons
+#if UNITY_2019_3_OR_NEWER
+			leftRect.xMin += space; // Spacing between tools and pivot
+#else
 			leftRect.xMin += largeSpace; // Spacing between tools and pivot
+#endif
 			leftRect.xMin += 64 * 2; // Pivot buttons
 			leftRect.xMax = playButtonsPosition;
 
@@ -80,7 +90,11 @@ namespace UnityToolbarExtender
 			rightRect.xMax -= dropdownWidth; // Layout
 			rightRect.xMax -= space; // Spacing between layout and layers
 			rightRect.xMax -= dropdownWidth; // Layers
+#if UNITY_2019_3_OR_NEWER
+			rightRect.xMax -= space; // Spacing between layers and account
+#else
 			rightRect.xMax -= largeSpace; // Spacing between layers and account
+#endif
 			rightRect.xMax -= dropdownWidth; // Account
 			rightRect.xMax -= space; // Spacing between account and cloud
 			rightRect.xMax -= buttonWidth; // Cloud
@@ -94,10 +108,17 @@ namespace UnityToolbarExtender
 			rightRect.xMax -= space;
 
 			// Add top and bottom margins
+#if UNITY_2019_3_OR_NEWER
+			leftRect.y = 4;
+			leftRect.height = 22;
+			rightRect.y = 4;
+			rightRect.height = 22;
+#else
 			leftRect.y = 5;
 			leftRect.height = 24;
 			rightRect.y = 5;
 			rightRect.height = 24;
+#endif
 
 			if (leftRect.width > 0)
 			{
