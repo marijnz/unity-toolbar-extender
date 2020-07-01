49bb2cb5033ea630b1581aa74dc4e1b5c98ec3e8
Lucas Pereira Vasconcelos <garrafote@gmail.com>
2020-07-01 11:05:04 -0300

    Added compatibility whith Unity 2020.1 - Fix: #15

--- Editor/ToolbarCallback.cs
+++ Editor/ToolbarCallback.cs
@@ -15,8 +15,16 @@ namespace UnityToolbarExtender
 	{
 		static Type m_toolbarType = typeof(Editor).Assembly.GetType("UnityEditor.Toolbar");
 		static Type m_guiViewType = typeof(Editor).Assembly.GetType("UnityEditor.GUIView");
+#if UNITY_2020_1_OR_NEWER
+		static Type m_iWindowBackendType = typeof(Editor).Assembly.GetType("UnityEditor.IWindowBackend");
+		static PropertyInfo m_windowBackend = m_guiViewType.GetProperty("windowBackend",
+			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+		static PropertyInfo m_viewVisualTree = m_iWindowBackendType.GetProperty("visualTree",
+			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+#else
 		static PropertyInfo m_viewVisualTree = m_guiViewType.GetProperty("visualTree",
 			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+#endif
 		static FieldInfo m_imguiContainerOnGui = typeof(IMGUIContainer).GetField("m_OnGUIHandler",
 			BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
 		static ScriptableObject m_currentToolbar;
@@ -42,8 +50,15 @@ namespace UnityToolbarExtender
 				m_currentToolbar = toolbars.Length > 0 ? (ScriptableObject) toolbars[0] : null;
 				if (m_currentToolbar != null)
 				{
+#if UNITY_2020_1_OR_NEWER
+					var windowBackend = m_windowBackend.GetValue(m_currentToolbar);
+
+					// Get it's visual tree
+					var visualTree = (VisualElement) m_viewVisualTree.GetValue(windowBackend, null);
+#else
 					// Get it's visual tree
 					var visualTree = (VisualElement) m_viewVisualTree.GetValue(m_currentToolbar, null);
+#endif
 
 					// Get first child which 'happens' to be toolbar IMGUIContainer
 					var container = (IMGUIContainer) visualTree[0];
