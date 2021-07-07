604e6299a1bc4942580ab2837abc616932202b0e
Marijn Zwemmer <marijn@marijnzwemmer.com>
2021-07-07 10:41:57 +0200

    Update ToolbarExtender.cs

--- Editor/ToolbarExtender.cs
+++ Editor/ToolbarExtender.cs
@@ -38,8 +38,9 @@ namespace UnityToolbarExtender
 			m_toolCount = toolIcons != null ? ((Array) toolIcons.GetValue(null)).Length : 5;
 #endif
 	
-			ToolbarCallback.OnToolbarGUI -= OnGUI;
-			ToolbarCallback.OnToolbarGUI += OnGUI;
+			ToolbarCallback.OnToolbarGUI = OnGUI;
+			ToolbarCallback.OnToolbarGUILeft = GUILeft;
+			ToolbarCallback.OnToolbarGUIRight = GUIRight;
 		}
 
 #if UNITY_2019_3_OR_NEWER
